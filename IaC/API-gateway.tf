27883aa8ff1333e4d98cf0e2609e30bd99c91dc1
SangWookie <swcube520@gmail.com>
2025-03-03 15:42:28 +0900

    feat: basic infra code

---
+++ IaC/API-gateway.tf
@@ -0,0 +1,80 @@
+variable "myregion" {
+    type = string
+    default = "ap-northeast-2"
+}
+
+variable "accountId" {
+    type = string
+    default = "954976299542"
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "myapi"
+  description = "myapi"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "resource"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "testLambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.12"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+# IAM
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "myrole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
---
+++ IaC/versions.tf
@@ -0,0 +1,38 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.37"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-venue-reservation"
+    key = "dev/terraform.tfstate"
+    region = "ap-northeast-2"
+
+    # State Locking
+    dynamodb_table = "venue_reservation_locking"
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "default"
+}
