e0cb24a646ada2546294cfc760bf0005846ec024
seonghyeoncho <chosh040@naver.com>
2025-03-10 20:26:23 +0900

    :sparkles: request manager start

---
+++ lambda_functions/request_manager/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sirupsen/logrus"
+	"request_manager/response"
+)
+
+var log = logrus.New()
+
+type RouteHandler func(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error)
+
+var routes = map[string]RouteHandler{}
+
+func handleRequest(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	log.Info("New request received")
+
+	var path = request.PathParameters["proxy"]
+
+	log.Info("Path parameters: ", path)
+	log.Info("Client IP: ", request.RequestContext.HTTP.SourceIP)
+
+	if handler, exist := routes[path]; exist {
+		return handler(request)
+	}
+
+	return response.APIGatewayResponseError("Internal Server Error", 500), nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
