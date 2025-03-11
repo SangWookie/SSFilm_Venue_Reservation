467d9e728cfbfa46c75f101c359de87629066919
SangWookie <swcube520@gmail.com>
2025-03-11 16:47:00 +0900

    feat: initial values for venue information

--- IaC/dynamodb.tf
+++ IaC/dynamodb.tf
@@ -28,6 +28,18 @@ resource "aws_dynamodb_table" "pending_reservation" {
   }
 }
 
+resource "aws_dynamodb_table" "reservation_limit" {
+  name           = "reservation_limit"
+  hash_key       = "venueDate"
+  billing_mode   = "PAY_PER_REQUEST"
+  stream_enabled = false
+
+  attribute {
+    name = "venueDate"
+    type = "S"
+  }
+}
+
 resource "aws_dynamodb_table" "venue_info" {
   name           = "venue_info"
   hash_key       = "venue"
@@ -40,14 +52,31 @@ resource "aws_dynamodb_table" "venue_info" {
   }
 }
 
-resource "aws_dynamodb_table" "reservation_limit" {
-  name           = "reservation_limit"
-  hash_key       = "venueDate"
-  billing_mode   = "PAY_PER_REQUEST"
-  stream_enabled = false
+variable "venues" {
+  type = list(object({
+    venue       = string
+    venueKor    = string
+    allowPolicy = string
+  }))
+  default = [
+    { venue = "studio", venueKor = "스튜디오", allowPolicy = "auto" },
+    { venue = "mastering1", venueKor = "마스터링룸 1", allowPolicy = "auto" },
+    { venue = "mastering2", venueKor = "마스터링룸 2", allowPolicy = "auto" },
+    { venue = "meeting", venueKor = "회의실", allowPolicy = "auto" },
+    { venue = "mixing", venueKor = "믹싱룸/ADR룸", allowPolicy = "auto" },
+    { venue = "editing", venueKor = "편집실", allowPolicy = "manual" },
+    { venue = "lounge", venueKor = "과방", allowPolicy = "manual" },
+  ]
+}
 
-  attribute {
-    name = "venueDate"
-    type = "S"
-  }
+resource "aws_dynamodb_table_item" "venue_init" {
+  for_each   = { for item in var.venues : item.venue => item }
+  table_name = aws_dynamodb_table.venue_info.name
+  hash_key   = "venue"
+
+  item = jsonencode({
+    "venue"       = { "S" = each.value.venue }
+    "venueKor"    = { "S" = each.value.venueKor }
+    "allowPolicy" = { "S" = each.value.allowPolicy }
+  })
 }
