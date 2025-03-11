1ec114529fedfe108689dbf466495f893a2de8ae
SangWookie <swcube520@gmail.com>
2025-03-11 11:29:14 +0900

    Revert "fix: delete datasource to untrack lambda changes"
    This reverts commit a7876b46e7ec19fa5622aac2f4fe6daf40694311.

--- IaC/lambda-config.tf
+++ IaC/lambda-config.tf
@@ -1,3 +1,10 @@
+# Lambda
+data "archive_file" "user_reservation_manager_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/user_reservation_manager"
+  output_path = "../lambda_functions/user_reservation_manager.zip"
+}
+
 resource "aws_lambda_function" "user_reservation_manager" {
   description   = "handles user reservation related requests"
   filename      = data.archive_file.user_reservation_manager_zip.output_path
@@ -5,6 +12,15 @@ resource "aws_lambda_function" "user_reservation_manager" {
   role          = aws_iam_role.sqs_lambda_role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.user_reservation_manager_zip.output_base64sha256
+}
+
+data "archive_file" "login_manager_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/login_manager"
+  output_path = "../lambda_functions/login_manager.zip"
 }
 
 resource "aws_lambda_function" "login_manager" {
@@ -14,6 +30,15 @@ resource "aws_lambda_function" "login_manager" {
   role          = aws_iam_role.role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.login_manager_zip.output_base64sha256
+}
+
+data "archive_file" "request_manager_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/request_manager"
+  output_path = "../lambda_functions/request_manager.zip"
 }
 
 resource "aws_lambda_function" "request_manager" {
@@ -23,6 +48,15 @@ resource "aws_lambda_function" "request_manager" {
   role          = aws_iam_role.role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.request_manager_zip.output_base64sha256
+}
+
+data "archive_file" "mode_manager_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/mode_manager"
+  output_path = "../lambda_functions/mode_manager.zip"
 }
 
 resource "aws_lambda_function" "mode_manager" {
@@ -32,6 +66,15 @@ resource "aws_lambda_function" "mode_manager" {
   role          = aws_iam_role.role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.mode_manager_zip.output_base64sha256
+}
+
+data "archive_file" "stat_handler_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/stat_handler"
+  output_path = "../lambda_functions/stat_handler.zip"
 }
 
 resource "aws_lambda_function" "stat_handler" {
@@ -41,6 +84,15 @@ resource "aws_lambda_function" "stat_handler" {
   role          = aws_iam_role.role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.stat_handler_zip.output_base64sha256
+}
+
+data "archive_file" "reservation_queue_handler_zip" {
+  type        = "zip"
+  source_dir  = "../lambda_functions/reservation_queue_handler"
+  output_path = "../lambda_functions/reservation_queue_handler.zip"
 }
 
 resource "aws_lambda_function" "reservation_queue_handler" {
@@ -50,6 +102,9 @@ resource "aws_lambda_function" "reservation_queue_handler" {
   role          = aws_iam_role.sqs_lambda_poll_role.arn
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
+
+  # comment this line to upload source code only once(untrack changes)
+  source_code_hash = data.archive_file.reservation_queue_handler_zip.output_base64sha256
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
