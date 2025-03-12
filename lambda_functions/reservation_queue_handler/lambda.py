74f0305d65a5fb32eefd2756106acd1dcce4148e
SangWookie <swcube520@gmail.com>
2025-03-12 14:18:28 +0900

    fix: try catch for debugging

--- lambda_functions/reservation_queue_handler/lambda.py
+++ lambda_functions/reservation_queue_handler/lambda.py
@@ -4,19 +4,25 @@ from reservationLimitHandler import reservationLimitHandler
 
 def lambda_handler(event, context):
     # event 객체에서 SQS 메시지를 추출
-    for record in event['Records']:
-        # SQS 메시지의 Body를 출력
-        body = record['body']
-        print(f"Received message: {body}")
+    try:
+        for record in event['Records']:
+            # SQS 메시지의 Body를 출력
+            body = record['body']
+            print(f"Received message: {body}")
 
-        requestId = record['md5OfBody']
-        
-        message = json.loads(body)
-        reservationLimitHandler(message, requestId)
-        
-        print(f"Processed message: {message}")
+            requestId = record['md5OfBody']
+            
+            message = json.loads(body)
+            reservationLimitHandler(message, requestId)
+            
+            print(f"Processed message: {message}")
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(message)
-    }
+        return {
+            'statusCode': 200,
+            'body': json.dumps(message)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({"error": str(e)}),
+        }
