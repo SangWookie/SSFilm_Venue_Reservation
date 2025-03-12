e663a3de8bd201f5d6c6f53579f14eb16e08a830
SangWookie <swcube520@gmail.com>
2025-03-12 13:43:20 +0900

    feat: add initial code for reservation_queue_handler

--- lambda_functions/reservation_queue_handler/lambda.py
+++ lambda_functions/reservation_queue_handler/lambda.py
@@ -1,8 +1,22 @@
 import json
+import boto3
+from reservationLimitHandler import reservationLimitHandler
 
 def lambda_handler(event, context):
-    # TODO implement
+    # event 객체에서 SQS 메시지를 추출
+    for record in event['Records']:
+        # SQS 메시지의 Body를 출력
+        body = record['body']
+        print(f"Received message: {body}")
+
+        requestId = record['md5OfBody']
+        
+        message = json.loads(body)
+        reservationLimitHandler(message, requestId)
+        
+        print(f"Processed message: {message}")
+
     return {
         'statusCode': 200,
-        'body': json.dumps('This is reservation_queue_handler!')
+        'body': json.dumps(message)
     }
