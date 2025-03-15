ee26464cca15c1be02f8fd7f7c7907e0a7df027c
SangWookie <swcube520@gmail.com>
2025-03-15 14:24:21 +0900

    feat: basic code for login

--- lambda_functions/login_manager/lambda.py
+++ lambda_functions/login_manager/lambda.py
@@ -1,8 +1,46 @@
 import json
+import jwt
+import datetime
+
+TOKEN_SECRET = "asdfqwer1234"
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('This is login_manager!')
+    body = event.get("body", {})
+    
+    try:
+        if(body.get("username") == "admin" and body.get("password") == "admin"):
+            
+            token = issue_access_token(body.get("username"))
+            
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'token': token}),
+                'headers': {
+                    'auth_token': token
+                }
+            }
+            
+        
+    except Exception as error:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(error)})
+        }
+
+def issue_access_token(username):
+    payload = {
+        "username": username
     }
+    payload["iat"] = datetime.datetime.now()
+    payload["exp"] = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    
+    token = jwt.encode(payload, TOKEN_SECRET, algorithm="HS256")
+    
+    return token
+
+def verify_access_token(token):
+    try:
+        payload = jwt.decode(token, TOKEN_SECRET, algorithms=["HS256"])
+        return payload
+    except Exception as error:
+        return None
