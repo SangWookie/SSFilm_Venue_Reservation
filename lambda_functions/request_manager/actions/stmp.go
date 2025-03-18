8a934cb451ab432725d23766e093411c93f09a17
seonghyeoncho <chosh040@naver.com>
2025-03-18 19:25:03 +0900

    :ambulance: 이메일 헤더 형식 수정

--- lambda_functions/request_manager/actions/stmp.go
+++ lambda_functions/request_manager/actions/stmp.go
@@ -34,6 +34,8 @@ func (r *SMTPManager) SendEmailWithGoogle(to, subject, body string) error {
 	msg := []byte(fmt.Sprintf(
 		"To: %s\r\n"+
 			"Subject: %s\r\n"+
+			"MIME-Version: 1.0\r\n"+
+			"Content-Type: text/html; charset=\"UTF-8\"\r\n"+
 			"\r\n"+
 			"%s\r\n", to, subject, body,
 	))
